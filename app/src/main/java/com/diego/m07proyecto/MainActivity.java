d264ac0ba67475934dedccd90691a6c77ff7854e
diego <diego_9696c@hotmail.com>
2019-12-13 15:27:04 +0100

    Creacion Activities

--- app/src/main/java/com/diego/m07proyecto/MainActivity.java
+++ app/src/main/java/com/diego/m07proyecto/MainActivity.java
@@ -2,13 +2,24 @@ package com.diego.m07proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences preferencias;
+    private String sharedPreFile = "com.diego.m07proyecto";
+    private final String USUARIO_KEY = "usuario";
+    private final String CLAVE_KEY = "clave";
+
+    public static final String KEY_INTENT_SEGUNDA_ACTIVITY = "keyEnviarSegundaActivity";
     private TextView textoUsuario;
     private TextView textoClave;
     private TextView textoRegistro;
@@ -25,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
         textoClave = findViewById(R.id.textoClave);
         textoRegistro = findViewById(R.id.textoRegistrar);
 
+        preferencias = getSharedPreferences(sharedPreFile, MODE_PRIVATE);
+
+        usuario = preferencias.getString(USUARIO_KEY, new String());
+        clave = preferencias.getString(CLAVE_KEY, new String());
+        textoUsuario.setText(usuario);
+        textoClave.setText(clave);
+
         textoRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,5 +53,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void iniciarSesion(View view) {
         Toast.makeText(getApplicationContext(), "INICIAR SESIOOON", Toast.LENGTH_LONG).show();
+    }
+
+/*
+    public void enviarEdadSegundaActivity(View view) {
+        if (!textoEdad.getText().toString().equals("") && Integer.parseInt(textoEdad.getText().toString()) > 1
+                && Integer.parseInt(textoEdad.getText().toString()) < 100) {
+            Intent intentSegundaActivity = new Intent(this, segundaActivity.class);
+            intentSegundaActivity.putExtra(KEY_INTENT_SEGUNDA_ACTIVITY, textoEdad.getText().toString());
+            textoEdad.setText("");
+            startActivity(intentSegundaActivity);
+        } else {
+            Toast.makeText(this, "Edat invalida...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+ */
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor preferencesEditor = preferencias.edit();
+        preferencesEditor.putString(USUARIO_KEY, usuario);
+        preferencesEditor.putString(CLAVE_KEY, clave);
+        preferencesEditor.apply();
     }
 }
