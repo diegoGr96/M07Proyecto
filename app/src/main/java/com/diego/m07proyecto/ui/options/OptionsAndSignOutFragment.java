d2eeca101e816732e4f14d23eb48b5501d8878c2
Diego Garcia <diego_9696c@hotmail.com>
2020-03-01 22:02:54 +0100

    'Filtrar Temas' y visualizar 'Mis Temas' funcionando correctamente.

--- app/src/main/java/com/diego/m07proyecto/ui/options/OptionsAndSignOutFragment.java
+++ app/src/main/java/com/diego/m07proyecto/ui/options/OptionsAndSignOutFragment.java
@@ -1,5 +1,6 @@
 package com.diego.m07proyecto.ui.options;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,24 +13,28 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.diego.m07proyecto.MainActivity;
 import com.diego.m07proyecto.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class OptionsAndSignOutFragment extends Fragment {
 
     private OptionsAndSignOutViewModel optionsAndSignOutViewModel;
+    FirebaseDatabase database;
+    FirebaseAuth mAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         optionsAndSignOutViewModel =
                 ViewModelProviders.of(this).get(OptionsAndSignOutViewModel.class);
         View root = inflater.inflate(R.layout.fragment_signout, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        optionsAndSignOutViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        startActivity(intent);
+        mAuth.signOut();
+        getActivity().finish();
         return root;
     }
 }
